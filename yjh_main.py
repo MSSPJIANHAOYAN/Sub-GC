1a6b11974b3bc12355efe8b892d471676b3ae030
miller <ryanyam524759288@gmail.com>
2022-04-25 17:45:52 +0800

    This is our project initial state.

--- yjh_main.py
+++ yjh_main.py
@@ -3,8 +3,28 @@
 #ix2word in coco talk, which doesn't exist in senticap.
 
 import json
+import h5py
 
 senticap_path = 'data/senticap_dataset.json'
+coco_path = 'data/cocotalk.json'
 senticap = json.load(open(senticap_path))
+cococap = json.load(open(coco_path))
 print(len(senticap['images']))
+print(cococap.keys())
+print(len(cococap['images']))
 print('done')
+cap_file = cococap['images']
+print(cap_file[0]['file_path'])
+a = senticap['images'][0]
+
+f = h5py.File('data/cocotalk_label.h5', 'r')
+print(f.keys())
+
+# for a, b in enumerate(cap_file):
+#     if a == 0:
+#         print(b)
+#         if 'COCO_val2014_000000391895.jpg' in b['file_path']:
+#             print('True')
+#
+
+
