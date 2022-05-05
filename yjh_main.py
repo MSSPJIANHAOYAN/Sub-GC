3f58f4057d938f09683db29e01874b4f0d298ea0
miller <ryanyam524759288@gmail.com>
2022-05-05 15:55:13 +0800

    This is our project initial state.

--- yjh_main.py
+++ yjh_main.py
@@ -4,50 +4,91 @@
 
 import json
 import h5py
-
+##Load Data
 senticap_path = 'data/senticap_dataset.json'
 coco_path = 'data/cocotalk.json'
 senticap = json.load(open(senticap_path))
 cococap = json.load(open(coco_path))
 
-senticap_new = {"images":
-                    [{"imgid": 31369, "sentences": [{"tokens": ["a", "plate", "of", "delicious", "food", "including", "french", "fries"],
-                                                     "word_sentiment": [0.0, 0.0, 0.0, 1, 1, 0.0, 0.0, 0.0], "sentiment": 1, "raw": "a plate of delicious food including French fries."}, {"tokens": ["french", "fries", "are", "not", "a", "healthy", "food", "but", "it", "is", "an", "excellent", "food", "for", "teenagers"], "word_sentiment": [0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0.0, 0.0, 0.0, 0.0, 1, 1, 0.0, 0.0], "sentiment": 1, "raw": "French fries are not a healthy food but it is an excellent food for teenagers."}, {"tokens": ["the", "plate", "has", "one", "of", "my", "favorite", "foods", "on", "it", "french", "fries"], "word_sentiment": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0.0, 0.0, 0.0, 0.0], "sentiment": 1, "raw": "The plate has one of my favorite foods on it, French fries."}, {"tokens": ["it", "was", "disgusting", "food", "not", "just", "bad", "food"], "word_sentiment": [0.0, 0.0, 1, 1, 0.0, 0.0, 1, 1], "sentiment": 0, "raw": "It was disgusting food, not just bad food."}, {"tokens": ["a", "plate", "of", "disgusting", "food", "found", "at", "a", "diner"], "word_sentiment": [0.0, 0.0, 0.0, 1, 1, 0.0, 0.0, 0.0, 0.0], "sentiment": 0, "raw": "A plate of disgusting food found at a diner."}, {"tokens": ["the", "meat", "on", "the", "burger", "looks", "like", "disgusting", "food"], "word_sentiment": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1], "sentiment": 0, "raw": "The meat on the burger looks like disgusting food."}], "split": "train", "filename": "COCO_val2014_000000389081.jpg"}, {"imgid": 39953, "sentences": [{"tokens": ["i", "make", "great", "coffee", "it", "is", "the", "best", "coffee", "because", "of", "my", "secret", "blend"], "word_sentiment": [0.0, 0.0, 1, 1, 0.0, 0.0, 0.0, 1, 1, 0.0, 0.0, 0.0, 0.0, 0.0], "sentiment": 1, "raw": "I make great coffee. It is the best coffee because of my secret blend."}, {"tokens": ["a", "clean", "sleek", "kitchen", "the", "sun", "streaming", "in", "through", "the", "tall", "window"], "word_sentiment": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "sentiment": 1, "raw": "A clean sleek kitchen the sun streaming in through the tall window."}, {"tokens": ["a", "beautiful", "wellappointed", "kitchen", "with", "a", "counter", "window"], "word_sentiment": [0.0, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 1], "sentiment": 1, "raw": "A beautiful well-appointed kitchen with a counter window."}, {"tokens": ["three", "ugly", "mugs", "are", "on", "the", "kitchen", "counter"], "word_sentiment": [0.0, 1, 1, 0.0, 0.0, 0.0, 0.0, 0.0], "sentiment": 0, "raw": "three ugly mugs are on the kitchen counter."}, {"tokens": ["i", "saw", "an", "ugly", "mug", "beside", "the", "dirty", "window"], "word_sentiment": [0.0, 0.0, 0.0, 1, 1, 0.0, 0.0, 1, 1], "sentiment": 0, "raw": "I saw an ugly mug beside the dirty window."}, {"tokens": ["a", "small", "kitchen", "has", "a", "checkered", "floor", "a", "window", "and", "an", "ugly", "mug", "on", "the", "counter"], "word_sentiment": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0.0, 0.0, 0.0], "sentiment": 0, "raw": "A small kitchen has a checkered floor, a window, and an ugly mug on the counter."}], "split": "train", "filename": "COCO_val2014_000000290768.jpg"}]}
-prev_idx_word = cococap['ix_to_word']
+images_info = cococap['images']
+senticap_info = senticap['images']
+new_info = []
+print('the length of senticap is', len(senticap_info))
+for sentivalues in senticap_info:
+    image_item = {}
+    file_name = sentivalues['filename']
+    split = sentivalues['split']
+    for cocovalues in images_info:
+        if file_name in cocovalues['file_path']:
+            image_item['id'] = cocovalues['id']
+            image_item['split'] = split
+            image_item['file_path'] = cocovalues['file_path']
+            new_info.append(image_item)
+
+print('len of new', len(new_info))
+# a = senticap['images']
+# new_senticap = {}
+# images = []
+# images_item = {}
+# senti_trial = a[0]
+# print(senti_trial)
+# file_name = senti_trial['filename']
+# split = senti_trial['split']
+# print(file_name)
+# for values in images_info:
+#     if file_name in values['file_path']:
+#         images_item['id'] = values['id']
+#         images_item['split'] = split
+#         images_item['file_path'] = values['file_path']
+
+# print(images_item)
+
+# for values in a:
+#     file_name = values['filename']
+#     split = values['split']
+#     for info_exam in images_info:
+#         if file_name in info_exam['file_path']:
+
+##This part aims to match images in senticap with original coco cap.
 
 
 
-
-word_comb = []
-
-for key, value in enumerate(prev_idx_word):
-    vals = prev_idx_word[value]
-    word_comb.append(vals)
-
-new_word = []
-a = senticap['images']
-for i, b in enumerate(a):
-    sentence = b['sentences']
-    for some in sentence:
-        tokens = some['tokens']
-        for item in tokens:
-            if item not in word_comb:
-                if item not in new_word:
-                    new_word.append(item)
+##This part aims to build vocabulary for senti cap based on existing volcabulary.
+# prev_idx_word = cococap['ix_to_word']
+#
+#
+#
+#
+# word_comb = []
+#
+# for key, value in enumerate(prev_idx_word):
+#     vals = prev_idx_word[value]
+#     word_comb.append(vals)
+#
+# new_word = []
+# a = senticap['images']
+# for i, b in enumerate(a):
+#     sentence = b['sentences']
+#     for some in sentence:
+#         tokens = some['tokens']
+#         for item in tokens:
+#             if item not in word_comb:
+#                 if item not in new_word:
+#                     new_word.append(item)
+# # #
 # #
+# #
+# #
+# # print(new_word)
+#
+# for i, values in enumerate(new_word):
+#     prev_idx_word[str(eval('9488+i'))] = values
 #
 #
-#
-# print(new_word)
-
-for i, values in enumerate(new_word):
-    prev_idx_word[str(eval('9488+i'))] = values
-
-
-print(prev_idx_word)
-print(len(prev_idx_word))
-print(len(new_word))
 # print(prev_idx_word)
+# print(len(prev_idx_word))
+# print(len(new_word))
+
 
 
 
