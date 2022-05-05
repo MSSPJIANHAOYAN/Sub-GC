e780241a92e0126ae99f0656e3cba9e5846d4b24
miller <ryanyam524759288@gmail.com>
2022-05-05 16:09:10 +0800

    This is our project initial state.

--- yjh_main.py
+++ yjh_main.py
@@ -26,69 +26,42 @@ for sentivalues in senticap_info:
             new_info.append(image_item)
 
 print('len of new', len(new_info))
-# a = senticap['images']
-# new_senticap = {}
-# images = []
-# images_item = {}
-# senti_trial = a[0]
-# print(senti_trial)
-# file_name = senti_trial['filename']
-# split = senti_trial['split']
-# print(file_name)
-# for values in images_info:
-#     if file_name in values['file_path']:
-#         images_item['id'] = values['id']
-#         images_item['split'] = split
-#         images_item['file_path'] = values['file_path']
-
-# print(images_item)
-
-# for values in a:
-#     file_name = values['filename']
-#     split = values['split']
-#     for info_exam in images_info:
-#         if file_name in info_exam['file_path']:
-
-##This part aims to match images in senticap with original coco cap.
 
 
 
 ##This part aims to build vocabulary for senti cap based on existing volcabulary.
-# prev_idx_word = cococap['ix_to_word']
-#
-#
-#
-#
-# word_comb = []
-#
-# for key, value in enumerate(prev_idx_word):
-#     vals = prev_idx_word[value]
-#     word_comb.append(vals)
-#
-# new_word = []
-# a = senticap['images']
-# for i, b in enumerate(a):
-#     sentence = b['sentences']
-#     for some in sentence:
-#         tokens = some['tokens']
-#         for item in tokens:
-#             if item not in word_comb:
-#                 if item not in new_word:
-#                     new_word.append(item)
-# # #
-# #
-# #
-# #
-# # print(new_word)
-#
-# for i, values in enumerate(new_word):
-#     prev_idx_word[str(eval('9488+i'))] = values
-#
-#
-# print(prev_idx_word)
-# print(len(prev_idx_word))
-# print(len(new_word))
+prev_idx_word = cococap['ix_to_word']
+word_comb = []
+print('previous', len(prev_idx_word))
+for key, value in enumerate(prev_idx_word):
+    vals = prev_idx_word[value]
+    word_comb.append(vals)
 
+new_word = []
+a = senticap['images']
+for i, b in enumerate(a):
+    sentence = b['sentences']
+    for some in sentence:
+        tokens = some['tokens']
+        for item in tokens:
+            if item not in word_comb:
+                if item not in new_word:
+                    new_word.append(item)
+
+#
+for i, values in enumerate(new_word):
+    prev_idx_word[str(eval('9488+i'))] = values
+
+print('Current', len(prev_idx_word))
+print(len(new_word))
+
+##Build new senticap json
+new_senticap_dataset = {}
+new_senticap_dataset['images'] = new_info
+new_senticap_dataset['ix_to_word'] = prev_idx_word
+
+with open("data/new_senticap_dataset.json", "w") as fp:
+    json.dump(new_senticap_dataset,fp)
 
 
 
